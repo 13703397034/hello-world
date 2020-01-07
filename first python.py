6679c1f88d302f8fe59dfb8f99602ae01884e06a
13703397034 <43983178+13703397034@users.noreply.github.com>
2020-01-07 19:29:39 +0800

    1

--- first python.py
+++ first python.py
@@ -2,7 +2,7 @@
 from openpyxl import Workbook
 #导入包注意大小写，定义一个workbook类
 wb = Workbook()
-#定位到工作表sheet
+#定位到工作表sheet(初始化一个sheet实例)
 ws = wb.active
 # idex 等同于索引标记
 ws1 = wb.create_sheet("mysheet1")# 插入到最后
@@ -14,3 +14,13 @@ ws.sheet_properties.tabColor="1072BA"#可以修改标签背景色为RRGGBB格式
 print(wb.sheetnames)# 打印所有sheet名
 for sheet in wb:      #遍历整个工作簿
     print(sheet.title)
+source = wb.active# 重新定义一个sheet实例化
+source1 = wb.copy_worksheet(ws2)#复制ws1到source1中
+print(source1.title)
+'''
+以上内容为操作工作簿(sheet)的操作
+'''
+c = ws['A5']
+ws['A5'] = 10000
+d = ws.cell(6,6,20000)
+
