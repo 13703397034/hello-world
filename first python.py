cf00772b2d901af519e6d633391f00a4c989542b
Administrator <274639586@qq.com>
2020-01-07 16:05:06 +0800

    first trust

--- first python.py
+++ first python.py
@@ -10,5 +10,7 @@ ws2 = wb.create_sheet("mysheet2",0)# 插入到最前面
 ws3 = wb.create_sheet("mysheet3",-1)# 插入到倒数第二个
 ws1.title="firstsheet"#修改sheet名称
 ws.sheet_properties.tabColor="1072BA"#可以修改标签背景色为RRGGBB格式的颜色
-#ws3 = wb["mysheeet3"]#可以将名称作为工作表的索引
-print(wb.sheetnames)
+#ws3 = wb["newtitle"]#可以将名称作为工作表的索引
+print(wb.sheetnames)# 打印所有sheet名
+for sheet in wb:      #遍历整个工作簿
+    print(sheet.title)
